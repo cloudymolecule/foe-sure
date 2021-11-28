b7fdec341e8183903b18b35083d6422aa3a78e44
Dario Carlino <baires82@gmail.com>
2021-11-28 14:13:09 -0500

    Update Parameters.js

--- src/components/Parameters.js
+++ src/components/Parameters.js
@@ -22,10 +22,24 @@ class parameters extends Component {
     render() {
         return (
             <div>
+                <p>The <i>seed</i> consists of a <b>10</b> numbers, and each number represents an attribute as follows:</p>
+                <ol>
+                    <li>Sex <small>(0: Female, 1: Male)</small></li>
+                    <li>Age <small>(0: Unknown, 1: Young, 2: Middle-aged, 3: Senior)</small></li>
+                    <li>Species <small>(0: Unknown, 1: Human, 2: Undead, 3: Mutant)</small></li>
+                    <li>Friendly <small>(0: Friendly, 1: Foe, 2: Neutral)</small></li>
+                    <li>Items <small>(0: None, 1: Some, 2: A lot)</small></li>
+                    <li>Strength <small>(0 - 9)</small></li>
+                    <li>Intelligence <small>(0 - 9)</small></li>
+                    <li>Charisma <small>(0 - 9)</small></li>
+                    <li>Endurance <small>(0 - 9)</small></li>
+                    <li>Name <small>(0 - 9)</small></li>
+                </ol>
                 <label>Seed:</label>
                 <input type='number' name='seed' onChange={this.handleChange} value={this.state.seed} placeholder='numbers only'/>
                 <button onClick={this.handleReset}>Accept</button>
                 <br/>
+                <br />
                 <button>Random Character</button>
             </div>
         )
