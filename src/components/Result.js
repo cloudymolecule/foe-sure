964d4f98f70aa804583c2bac72290f3db2b931c3
Dario Carlino <baires82@gmail.com>
2021-11-29 23:19:42 -0500

    Change yet another functional component to a a class component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,23 @@
-import React from 'react'
 import '../styles/result.css'
+import React, { Component } from 'react'
 
-function Result() {
-    return (
-        <div>
-            result
-        </div>
-    )
+class Result extends Component {
+    
+    renderResult = () => {
+        if (this.props.hello) {
+            return <h3>{this.props.hello}</h3>
+        } else {
+            return <h3>Nothing here</h3>
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderResult()}
+            </div>
+        )
+    }
 }
 
 export default Result
--- src/containers/Foe.js
+++ src/containers/Foe.js
@@ -6,15 +6,24 @@ import Result from '../components/Result'
 
 class Foe extends Component {
     
+    state = {
+        res: ''
+    }
+    
     processChar = val => {
+        this.setState({
+            res: val
+        })
         console.log(val)
     }
+
+    
     
     render() {
         return (
             <>
                 <Parameters charValues={this.processChar}/>
-                <Result />
+                <Result hello={this.state.res}/>
             </>
         )
     }
