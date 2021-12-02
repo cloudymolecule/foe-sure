72121c165e76c5fdbec0444ff68fa7fc6cc57fa0
Cloudymolecule <baires82@gmail.com>
2021-12-02 14:01:40 -0500

    Update names.js

--- src/constants/names.js
+++ src/constants/names.js
@@ -1,25 +1,26 @@
-export const boyNames = [
-    ['If you can read this, then something broke'],
-    ['X', 'W', 'T'], //=> names with 1 letter
-    ['Bo', 'Cy', 'Ed'], //=> names with 2 letters
-    ['Eli', 'Leo', 'Ian'], //=> names with 3 letters
-    ['Noah', 'Ezra', 'Cole'], //=> names with 4 letters
-    ['Mason', 'David', 'Avery'], //=> names with 5 letters
-    ['Carter', 'Samuel', 'Nathan'], //=> names with 6 letters
-    ['William', 'Michael', 'Dominic'], //=> names with 7 letters
-    ['Benjamin', 'Nicholas', 'Giovanni'], //=> names with 8 letters
-    ['Alexander', 'Sebastian', 'Valentino'] //=> names with 9 letters
-]
-
-export const girlNames = [
-    ['If you can read this, then something broke'],
-    ['M', 'L', 'Y'], //=> names with 1 letter
-    ['Di', 'Em', 'Io'], //=> names with 2 letters
-    ['Ava', 'Lea', 'Gia'], //=> names with 3 letters
-    ['Aria', 'Zoey', 'Eden'], //=> names with 4 letters
-    ['Emily', 'Sofia', 'Lydia'], //=> names with 5 letters
-    ['Olivia', 'Amalia', 'Sophia'], //=> names with 6 letters
-    ['Natalie', 'Madison', 'Lillian'], //=> names with 7 letters
-    ['Isabella', 'Victoria', 'Serenity'], //=> names with 8 letters
-    ['Charlotte', 'Annabelle', 'Valentina'] //=> names with 9 letters
+export const names = [
+    [
+        ['If you can read this, then something broke'],
+        ['M', 'L', 'Y'], //=> names with 1 letter
+        ['Di', 'Em', 'Io'], //=> names with 2 letters
+        ['Ava', 'Lea', 'Gia'], //=> names with 3 letters
+        ['Aria', 'Zoey', 'Eden'], //=> names with 4 letters
+        ['Emily', 'Sofia', 'Lydia'], //=> names with 5 letters
+        ['Olivia', 'Amalia', 'Sophia'], //=> names with 6 letters
+        ['Natalie', 'Madison', 'Lillian'], //=> names with 7 letters
+        ['Isabella', 'Victoria', 'Serenity'], //=> names with 8 letters
+        ['Charlotte', 'Annabelle', 'Valentina'] //=> names with 9 letters
+    ],
+    [
+        ['If you can read this, then something broke'],
+        ['X', 'W', 'T'], //=> names with 1 letter
+        ['Bo', 'Cy', 'Ed'], //=> names with 2 letters
+        ['Eli', 'Leo', 'Ian'], //=> names with 3 letters
+        ['Noah', 'Ezra', 'Cole'], //=> names with 4 letters
+        ['Mason', 'David', 'Avery'], //=> names with 5 letters
+        ['Carter', 'Samuel', 'Nathan'], //=> names with 6 letters
+        ['William', 'Michael', 'Dominic'], //=> names with 7 letters
+        ['Benjamin', 'Nicholas', 'Giovanni'], //=> names with 8 letters
+        ['Alexander', 'Sebastian', 'Valentino'] //=> names with 9 letters
+    ]
 ]
