a9797a1750776ac0701ac3bc228a123911cebce6
Cloudymolecule <baires82@gmail.com>
2021-12-02 10:18:04 -0500

    Update Foe.js

--- src/containers/Foe.js
+++ src/containers/Foe.js
@@ -11,10 +11,49 @@ class Foe extends Component {
     }
     
     processChar = n => {
-        this.generateCharacter(n)
+        const num = this.checkNumber(n)
+        console.log(num)
+        // this.generateCharacter(n)
         // this.setState({
         //     res: n
         // })
+    }
+
+    checkNumber = n => {
+        let res
+
+        function getRandomNumber() {
+            
+            let randNum = []
+
+            for (let i = 0; i < 10; i++) {
+                switch(i) {
+                    case 0:
+                        randNum.push(Math.round(Math.random()))
+                    case 1:
+                        randNum.push(Math.floor(Math.random() * 4))
+                    case 2:
+                        randNum.push(Math.floor(Math.random() * 4))
+                    case 3:
+                        randNum.push(Math.floor(Math.random() * 3))
+                    case 4:
+                        randNum.push(Math.floor(Math.random() * 3))
+                    default:
+                        randNum.push(Math.floor(Math.random() * 10))
+                }
+            }
+            console.log(randNum)
+        }
+
+
+        if (n.toString().length === 10) {
+            res = n
+        } else {
+            res = getRandomNumber()
+        }
+        // console.log(res)
+        return res
+
     }
 
     generateCharacter = n => {
